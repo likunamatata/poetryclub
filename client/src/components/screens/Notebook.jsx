7099e10c1f5ed7613c82f76743f96ea9b6b0ef6d
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-08-24 23:34:19 -0400

    added liked poems and my poems to notebook

--- client/src/components/screens/Notebook.jsx
+++ client/src/components/screens/Notebook.jsx
@@ -1,29 +1,51 @@
 import React, { Component } from "react";
 import { getUserPoems } from "../../services/poem-helpers";
 import Poems from "../widgets/Poems"
+import PoemsLiked from "../widgets/PoemsLiked"
+import { getLikes } from "../../services/like-helpers"
 
 class Notebook extends Component {
   constructor(props) {
     super(props);
     this.state = {
       poems: [],
+      likes: []
     };
   }
 
   componentDidMount = async () => {
-    const res = await getUserPoems(this.props.currentUser.id);
+    const userPoems = await getUserPoems(this.props.currentUser.id);
+    const userLikes = await getLikes()
     this.setState({
-      poems: res.data,
+      poems: userPoems,
+      likes: userLikes
     });
   };
 
+
   render() {
-    const poems = (this.state.poems.length === 0
-      ? ""
-      : <Poems poems={this.state.poems} currentUser={this.props.currentUser}/>);
+    const poems = (
+      this.state.poems.length === 0
+        ? ""
+        :
+        <div>
+          <h1>My Poems</h1>
+          <Poems poems={this.state.poems} currentUser={this.props.currentUser} />
+        </div>
+    )
+    const likes = (
+      this.state.likes.length === 0
+        ? ""
+        :
+        <div>
+          <h1>Poems That I Like</h1>
+          <PoemsLiked likes={this.state.likes} currentUser={this.props.currentUser} />
+        </div>
+    )
     return (
       <div className="notebook">
         {poems}
+        {likes}
       </div>
     );
   }
---
+++ client/src/components/widgets/PoemsLiked.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+// import styles from '../../styles/PoemsContainer.module.css'
+import styles from "../../styles/Poem.module.css";
+import { Link } from 'react-router-dom';
+
+
+class PoemsLiked extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+
+    const { likes, currentUser } = this.props
+    console.log(currentUser)
+
+    const likesData = likes.filter((liked) => liked.user_id === currentUser.id)
+    console.log(likesData)
+
+    const likedPoems = likesData.map((poem, id) => {
+      const textParsed = JSON.parse(poem.text).blocks[0].text
+      return (
+        <div key={id} className={styles.poem}>
+          <Link to={`/poems/${poem.poem_id}`}>
+            <p>{poem.title}</p>
+            <p className={styles.poemSnippet}>{`${textParsed}...`}</p>
+            <p>{`By ${poem.username}`}</p>
+          </Link>
+          <hr />
+        </div >
+
+      )
+    })
+
+    return (
+      <>
+        {likedPoems}
+      </>
+    );
+  }
+}
+
+
+export default PoemsLiked
