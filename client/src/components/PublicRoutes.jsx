9ae793227ceb64b6431241f24873458d46ec6a46
Lia Kapanadze <liakapanadze@Lias-MacBook.fios-router.home>
2020-08-29 16:03:44 -0400

    first try, made poems/poem_id public

---
+++ client/src/components/PublicRoutes.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Route } from "react-router-dom";
+
+import Feed from "./screens/Feed";
+import Notebook from "./screens/Notebook";
+import PoemLarge from "./widgets/PoemLarge";
+import Landing from "./Landing"
+
+function PublicScreens(props) {
+  const currentUser= props.currentUser
+  return (
+    <div>
+
+      {/* Feed */}
+       
+      {/* Search */}
+      <Route
+              exact
+              path="/"
+              render={() => (
+                <Landing
+                  history={props.history}
+                  handleLogin={props.handleLogin}
+                  loginHandleChange={props.loginHandleChange}
+                  registerHandleChange={props.registerHandleChange}
+                  loginFormData={props.loginFormData}
+                  registerFormData={props.registerFormData}
+                  currentUser={props.currentUser}
+                  handleRegister={props.handleRegister}
+                />
+              )}
+      />
+      
+      <Route
+        exact
+        path="/poems/:poem_id"
+        render={(props) => {
+          const { poem_id } = props.match.params;
+          return <PoemLarge poem_id={poem_id} history={props.history} currentUser={currentUser} />;
+        }}
+      />
+    </div>
+  );
+}
+
+export default PublicScreens;
