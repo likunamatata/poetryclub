8b0c16622aefd86036273b33fda21657b8adda07
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-09-23 00:47:00 -0400

    small fixes with main modal

--- client/src/components/widgets/modals-and-materials/MainModal.jsx
+++ client/src/components/widgets/modals-and-materials/MainModal.jsx
@@ -5,11 +5,15 @@ export default function MainModal(props) {
   return (
     <div className="w3-modal w3-animate-opacity" style={props.mainOptions ? { display: 'block' } : { display: 'none' }}>
       <div className="w3-modal-content w3-card-1">
-        <header className="w3-container w3-white">
+        <header className="header-container w3-white">
+          {/* HERE So the X shows on the right */}
+          <span style={{ visibility: 'hidden' }}>
+          </span>
+          {/* CLOSE MODAL ICON */}
           <svg onClick={() => {
             props.hideMainOptions()
           }}
-            width="43" height="54" viewBox="0 0 43 54" fill="none" xmlns="http://www.w3.org/2000/svg">
+            width="43" height="54" viewBox="0 0 43 43" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M31.6699 12.9094L30.1498 11.3823L21.4999 20.0729L12.8501 11.3823L11.33 12.9094L19.9799 21.6L11.33 30.2905L12.8501 31.8176L21.4999 23.1271L30.1498 31.8176L31.6699 30.2905L23.02 21.6L31.6699 12.9094Z" fill="black" />
           </svg>
         </header>
