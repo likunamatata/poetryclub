3da7e4a52b5efa9592277090b998e98cc1ba5be2
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2020-09-23 12:34:48 -0400

    search button matches figma. search bar on focus upon rendering

--- client/src/components/widgets/SearchBar.jsx
+++ client/src/components/widgets/SearchBar.jsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import styles from '../../styles/Search.module.css'
 
 function SearchBar(props) {
-    return (
-      <div className={styles.container}>
-        <form className={styles.authForm} onSubmit={(e) => {
-          e.preventDefault();
-          // REMOVED SEARCH OPTION. NO NEED FOR THIS SUBMIT BTN OR FUNCTION
-          // SEARCHING ON CHANGE NOW. But uncomment below if needed.
-          // props.handleSearch();
-        }} >
-          <input
-            className={styles.input}
-            name="keyword"
-            type="text"
-            onChange={props.handleChange}
-          />
-          { window.location.pathname === '/search' ?
-            <input className={styles.submit} type="submit" value="Search" />
-            :
-            null
-          }
-        </form>
-      </div>
-    );
-  }
+
+  const inputRef = useRef();
+
+  // FOR CURSOR ON EDITOR
+  useEffect(() => {
+    inputRef.current.focus();
+  },[])
+
+
+  return (
+    <div className={styles.container}>
+      <form className={styles.authForm} onSubmit={(e) => {
+        e.preventDefault();
+        // REMOVED SEARCH OPTION. NO NEED FOR THIS SUBMIT BTN OR FUNCTION
+        // SEARCHING ON CHANGE NOW. But uncomment below if needed.
+        // props.handleSearch();
+      }} >
+        <input
+          className={styles.input}
+          name="keyword"
+          type="text"
+          ref={inputRef}
+          onChange={props.handleChange}
+        />
+        {window.location.pathname === '/search' ?
+          <input className={styles.submit} type="submit" value="Search" />
+          :
+          null
+        }
+      </form>
+    </div>
+  );
+}
 
 export default SearchBar;
